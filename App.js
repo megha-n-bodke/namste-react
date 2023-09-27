f3a5ad0cf586ad6fac1578e7e9e62de4293d3217
Megha Bodke <megha.n.bodke@gmail.com>
2023-09-27 14:46:51 +0530

    Food app start

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const heading = React.createElement(
-  "h1",
+//const heading = React.createElement(
+/*   "h1",
   {
     id: "Heading",
   },
@@ -20,11 +20,33 @@ const element = (
   </h1>
 );
 console.log(element);
-
+ */
 // const parent = React.createElement("div", { id: "parent" }, [
 //   React.createElement("h1", { id: "heading" }, "Child Heading"),
 //   React.createElement("h2", { id: "Sibling" }, "Sibling heading"),
 // ]);
+
+const Heading = () => {
+  return (
+    <div>
+      <div className="logo">
+        <img
+          src="https://www.clipartmax.com/png/small/155-1558209_take-away-food-logos.png"
+          alt="Take Away Food Logos @clipartmax.com"
+        ></img>
+      </div>
+      <div className="menu">
+        <ul>
+          <li>Home </li>
+          <li>About Us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(element);
+root.render(<Heading />);
